781f182b5c8a96249c7bf6f7416a53a5aa5fe9a7
shdukane <shailendra.dukane@gep.com>
2020-02-05 15:18:00 +0530

    Revert "Merge pull request #1 from ShailendraDukane-Gep/develop2.0"
    This reverts commit e3454f2dd57ddd7fb88b2c02ca0b57ed997eccae, reversing
    changes made to c8e3cc5838e42a7617cd10fd0f873f7690b970d0.

--- Test1.cs
+++ Test1.cs
@@ -3,10 +3,6 @@ using System.Threading.Tasks;
 
 namespace demo1
 {
-
-// Test1 story is under development
-// Test1 story is still under development - Shailendra
-
     public class ApplicationHub : Hub
      { 
          public Task Send(string message){
